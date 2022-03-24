5864d92cbc77298b454c86257e447ecc30f7c608
Eternity <72909319+rizzyneck@users.noreply.github.com>
2022-03-24 17:15:02 +0700

    add: jarak-tempuh.cpp

---
+++ jarak-tempuh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float v,s,t;
+	cout<<"Penghitung Jarak Tempuh"<<endl;
+	cout<<"-----------------------------------"<<endl;
+	cout<<"Masukan Kecepatan (Km/jam) : ";
+	cin>>v;
+	cout<<"Masukan Waktu Tempuh (Jam) : ";
+	cin>>t;
+	s=v*t;
+	cout<<"Jarak (Km)                 : "<<s<<endl;
+}
